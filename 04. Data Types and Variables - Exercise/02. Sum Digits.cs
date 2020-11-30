5d331037ca4fc8dabcafbfe5b60c7f5f581727e3
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 22:45:11 +0200

    Create 02. Sum Digits.cs

---
+++ 04. Data Types and Variables - Exercise/02. Sum Digits.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Security.Cryptography;
+
+namespace BasicSyntaxDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
