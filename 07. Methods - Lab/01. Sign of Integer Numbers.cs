1555efe4206ca2332507c85095d613629dbb1fe3
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 23:06:17 +0200

    Create 01. Sign of Integer Numbers.cs

---
+++ 07. Methods - Lab/01. Sign of Integer Numbers.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace _01.SignOfInetegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintSign(n);
+
+        }
+
+        static void PrintSign(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+
+        }
+    }
+}
