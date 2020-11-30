36a51162997334813ad00ab2905ecd6eaf3460f2
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 23:25:22 +0200

    Create 04. List of Products.cs

---
+++ 09. Lists - Lab/04. List of Products.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.ListОfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                products.Add(Console.ReadLine());
+             
+            }
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+       }
+    }
+}
