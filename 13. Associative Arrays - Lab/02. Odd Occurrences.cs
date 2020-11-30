c460fc5a2f82a8691caf7e7a7b2f5557c592b42b
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 23:44:14 +0200

    Create 02. Odd Occurrences.cs

---
+++ 13. Associative Arrays - Lab/02. Odd Occurrences.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace _02.OddOccurences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                string wordInLowerCase = word.ToLower();
+                if (counts.ContainsKey(wordInLowerCase))
+                {
+                    counts[wordInLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordInLowerCase, 1);
+                }   
+            }
+
+            foreach (var count in counts)
+            {
+                if (!(count.Value % 2 == 0))
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
