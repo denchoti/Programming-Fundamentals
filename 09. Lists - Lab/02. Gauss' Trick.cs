0879b73f103878d38ae2bff2b344697948598023
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 23:24:48 +0200

    Create 02. Gauss' Trick.cs

---
+++ 09. Lists - Lab/02. Gauss' Trick.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = ReadlistSingleLine();
+            Console.WriteLine(String.Join(" ",SumPairs(list)));
+        }
+        static List<int> SumPairs(List<int> numbers)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < numbers.Count/2; i++)
+            {
+                result.Add(numbers[i] + numbers[numbers.Count - i - 1]);
+            }
+            if (numbers.Count % 2 == 1)
+            {
+                result.Add(numbers[numbers.Count / 2]);
+            }
+            return result;
+        }
+        static List<int> ReadlistSingleLine()
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            return list;
+        }
+    }
+}
