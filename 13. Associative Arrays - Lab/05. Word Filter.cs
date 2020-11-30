7ffda5ba883cb6a2d94d549162024475f2468af7
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 23:45:26 +0200

    Create 05. Word Filter.cs

---
+++ 13. Associative Arrays - Lab/05. Word Filter.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Linq;
+
+namespace _05.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Where(n => n.Length % 2 == 0).ToArray();
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+           
+        }
+    }
+}
