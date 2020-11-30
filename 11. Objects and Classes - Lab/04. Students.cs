8e3c8b1e8f303a22909ef7fd6a2449ed01512470
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 23:37:57 +0200

    Create 04. Students.cs

---
+++ 11. Objects and Classes - Lab/04. Students.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Students
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string input = Console.ReadLine();
+
+            while (input != "end")
+            {
+                string[] tokens = input.Split();
+
+                string firstName = tokens[0];
+                string lastName = tokens[1];
+                int age = int.Parse(tokens[2]);
+                string city = tokens[3];
+
+                Student student = new Student()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    City = city
+                };
+
+                students.Add(student);
+                input = Console.ReadLine();
+            }
+
+            string filterCity = Console.ReadLine();
+
+
+            List<Student> filteredStudents = students.Where(n => n.City == filterCity).ToList();
+            foreach (Student student in filteredStudents) // !filteredStudents not students
+            {
+                Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+            }
+        }
+    }
+
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
