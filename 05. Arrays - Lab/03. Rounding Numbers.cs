d01a5029ae178350b0fb77572edab25bdc637391
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 22:55:05 +0200

    Create 03. Rounding Numbers.cs

---
+++ 05. Arrays - Lab/03. Rounding Numbers.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace _03.RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            double[] numbers = new double[input.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = double.Parse(input[i]);
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == 0)
+                {
+                    numbers[i] = 0;
+                }
+                Console.WriteLine($"{numbers[i]} => {(int)Math.Round(numbers[i],MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
