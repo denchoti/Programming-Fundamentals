5de6a9999eba9c95741f689630398aa8aa6cd3b6
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-11-30 22:37:32 +0200

    Create 09. Chars to String.cs

---
+++ 03. Data Types and Variables - Lab/09. Chars to String.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _09.CharToString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+            string third = Console.ReadLine();
+
+            Console.WriteLine(first+second+third);
+        }
+    }
+}
