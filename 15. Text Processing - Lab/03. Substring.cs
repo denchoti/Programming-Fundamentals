3de5968147896904a0e1347954790b01804e194e
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-12-01 15:29:03 +0200

    Create 03. Substring.cs

---
+++ 15. Text Processing - Lab/03. Substring.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace _03.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int index = text.IndexOf(word);
+            while (index >= 0)
+            {
+                text = text.Remove(index, word.Length);
+                index = text.IndexOf(word);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
