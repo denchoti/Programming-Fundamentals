0274c3d59addbdcb5326b098b5f3e07920b1eba8
Denitsa <65463547+denchoti@users.noreply.github.com>
2020-12-01 15:28:42 +0200

    Create 02. Repeat Strings.cs

---
+++ 15. Text Processing - Lab/02. Repeat Strings.cs
@@ -0,0 +1,25 @@
+sing System;
+using System.Text;
+
+namespace _02.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            StringBuilder result = new StringBuilder();
+            foreach (string word in words)
+            {
+                int count = word.Length;
+
+                for (int i = 0; i < count; i++)
+                {
+                    result.Append(word);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
